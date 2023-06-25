974b9d95bd000007ee1c9e4fa27b86ad551a1089
notsatria <buanasatriaa@gmail.com>
2023-06-25 23:18:36 +0700

    Added new page: User registration

---
+++ registrasi-user.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <!-- Meta, title, CSS, favicons, etc. -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="icon" href="images/dashboard-logo.png" type="image/ico" />
+
+  <title>Monitoring | Register</title>
+
+  <!-- Bootstrap -->
+  <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <!-- Font Awesome -->
+  <link href="vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+  <!-- NProgress -->
+  <link href="vendors/nprogress/nprogress.css" rel="stylesheet" />
+  <!-- iCheck -->
+  <link href="vendors/iCheck/skins/flat/green.css" rel="stylesheet" />
+
+  <!-- bootstrap-progressbar -->
+  <link href="vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" />
+  <!-- JQVMap -->
+  <link href="vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
+  <!-- bootstrap-daterangepicker -->
+  <link href="vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet" />
+
+  <!-- Custom Theme Style -->
+  <link href="build/css/custom.min.css" rel="stylesheet" />
+
+  <style>
+    .form-container {
+      max-width: 400px;
+      margin: 0 auto;
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    .form-group {
+      margin-bottom: 20px;
+    }
+
+    .form-group label {
+      display: block;
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+
+    .form-group input,
+    .form-group select {
+      width: 100%;
+      padding: 10px;
+      font-size: 16px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+    }
+
+    .form-group .btn-submit {
+      background-color: #1ABB9C;
+      color: white;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+  </style>
+</head>
+
+<body class="nav-md">
+  <div class="container body">
+    <div class="main_container">
+      <div class="col-md-3 left_col">
+        <div class="left_col scroll-view">
+          <div class="navbar nav_title" style="border: 0">
+            <a href="index.html" class="site_title"><span>Dashboard</span></a>
+          </div>
+
+          <div class="clearfix"></div>
+          <!-- menu profile quick info -->
+          <div class="profile clearfix">
+
+            <div class="profile_info">
+              <span>Selamat Datang,</span>
+              <h2></h2>
+            </div>
+          </div>
+          <!-- /menu profile quick info -->
+
+          <br />
+
+          <!-- sidebar menu -->
+          <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+            <div class="menu_section">
+              <h3>Menu</h3>
+              <ul class="nav side-menu">
+                <li>
+                  <a href="home.php"><i class="fa fa-home"></i> Home </a>
+                </li>
+                <li>
+                  <a href="dashboard.php"><i class="fa fa-bar-chart-o"></i> Dashboard </a>
+                </li>
+
+                <li>
+                  <a href="data-ayam.php"><i class="fa fa-desktop"></i> Suhu Ayam </a>
+                </li>
+                <li>
+                  <a href="kandang-ayam.php"><i class="fa fa-desktop"></i> Suhu Kandang Ayam </a>
+                </li>
+                <li>
+                  <a href="registrasi-user.php"><i class="fa fa-user"></i> Tambah User </a>
+                </li>
+              </ul>
+            </div>
+            <div class="menu_section"></div>
+          </div>
+          <!-- /sidebar menu -->
+
+          <!-- /menu footer buttons -->
+          <div class="sidebar-footer hidden-small">
+            <form action="php/logout.php" method="POST">
+              <button data-toggle="tooltip" data-placement="top" title="Logout" type="submit" class="btn btn-danger btn-block">
+                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+                <span>Logout</span>
+              </button>
+            </form>
+          </div>
+          <!-- /menu footer buttons -->
+        </div>
+      </div>
+
+      <!-- top navigation -->
+      <div class="top_nav">
+        <div class="nav_menu">
+          <div class="nav toggle py-2">
+            <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+          </div>
+        </div>
+      </div>
+      <!-- /top navigation -->
+
+      <!-- page content -->
+      <div class="right_col" role="main">
+        <div class="m-5" style="color: transparent;">.</div>
+        <div class="form-container">
+          <h2>Tambah User Baru</h2>
+          <form action="proses_tambah_user.php" method="POST">
+            <div class="form-group">
+              <label for="nama_lengkap">Nama Lengkap:</label>
+              <input type="text" id="nama_lengkap" name="nama_lengkap" required>
+            </div>
+            <div class="form-group">
+              <label for="username">Username:</label>
+              <input type="text" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+              <label for="password">Password:</label>
+              <input type="password" id="password" name="password" required>
+            </div>
+            <div class="form-group">
+              <label for="role">Role:</label>
+              <select id="role" name="role">
+                <option value="karyawan">Karyawan</option>
+                <option value="admin">Admin</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <input type="submit" value="Tambah User" class="btn-submit">
+            </div>
+          </form>
+        </div>
+
+      </div>
+      <!-- /page content -->
+
+      <!-- footer content -->
+      <footer>
+        <div class="pull-right">Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a></div>
+        <div class="clearfix"></div>
+      </footer>
+      <!-- /footer content -->
+    </div>
+  </div>
+
+  <!-- jQuery -->
+  <script src="vendors/jquery/dist/jquery.min.js"></script>
+  <!-- Bootstrap -->
+  <script src="vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- FastClick -->
+  <script src="vendors/fastclick/lib/fastclick.js"></script>
+  <!-- NProgress -->
+  <script src="vendors/nprogress/nprogress.js"></script>
+  <!-- Chart.js -->
+  <script src="vendors/Chart.js/dist/Chart.min.js"></script>
+  <!-- gauge.js -->
+  <script src="vendors/gauge.js/dist/gauge.min.js"></script>
+  <!-- bootstrap-progressbar -->
+  <script src="vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+  <!-- iCheck -->
+  <script src="vendors/iCheck/icheck.min.js"></script>
+  <!-- Skycons -->
+  <script src="vendors/skycons/skycons.js"></script>
+  <!-- Flot -->
+  <script src="vendors/Flot/jquery.flot.js"></script>
+  <script src="vendors/Flot/jquery.flot.pie.js"></script>
+  <script src="vendors/Flot/jquery.flot.time.js"></script>
+  <script src="vendors/Flot/jquery.flot.stack.js"></script>
+  <script src="vendors/Flot/jquery.flot.resize.js"></script>
+  <!-- Flot plugins -->
+  <script src="vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+  <script src="vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+  <script src="vendors/flot.curvedlines/curvedLines.js"></script>
+  <!-- DateJS -->
+  <script src="vendors/DateJS/build/date.js"></script>
+  <!-- JQVMap -->
+  <script src="vendors/jqvmap/dist/jquery.vmap.js"></script>
+  <script src="vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+  <script src="vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+  <!-- bootstrap-daterangepicker -->
+  <script src="vendors/moment/min/moment.min.js"></script>
+  <script src="vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+  <!-- Custom Theme Scripts -->
+  <script src="build/js/custom.min.js"></script>
+</body>
+
+</html>
